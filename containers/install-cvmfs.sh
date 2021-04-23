7384d82b9a177a89a415dce6f3dcf99f2bf39eab
Bob Dröge <b.e.droge@rug.nl>
2021-04-23 17:26:59 +0200

    pass version to install script, copy rpms to container

--- containers/install-cvmfs.sh
+++ containers/install-cvmfs.sh
@@ -1,9 +1,9 @@
-cvmfsversion=2.8.1
+cvmfsversion=$1
 arch=$(uname -m)
 
 if [ "$arch" = "ppc64le" ]
 then
-    yum install -y ./ppc64le/cvmfs-2.8.1-1.el7.ppc64le.rpm ./ppc64le/cvmfs-fuse3-2.8.1-1.el7.x86_64.rpm
+    yum install -y /RPMS/ppc64le/cvmfs-${cvmfsversion}-1.el7.ppc64le.rpm ./ppc64le/cvmfs-fuse3-${cvmfsversion}-1.el7.ppc64le.rpm
 else
     yum install -y http://cvmrepo.web.cern.ch/cvmrepo/yum/cvmfs-release-latest.noarch.rpm \
     && yum install -y cvmfs-${cvmfsversion} cvmfs-config-default cvmfs-fuse3 sudo vim openssh-clients
