df83e3c2f7f564656cea6ec058f4c772c2cf6777
Bob Dröge <b.e.droge@rug.nl>
2021-04-23 12:12:13 +0200

    install our custom built RPM

--- containers/install-cvmfs.sh
+++ containers/install-cvmfs.sh
@@ -3,22 +3,7 @@ arch=$(uname -m)
 
 if [ "$arch" = "ppc64le" ]
 then
-    yum install -y cmake wget make unzip patch valgrind bzip2 gcc gcc-c++ sqlite-devel python-devel \
-                   libcap-devel libuuid-devel attr fuse-devel fuse3-devel zlib-devel openssl-devel
-
-    # build CernVM-FS from source (no aarch64 Debian packages available)
-    wget https://github.com/cvmfs/cvmfs/archive/cvmfs-${cvmfsversion}.tar.gz && \
-    tar xfz cvmfs-${cvmfsversion}.tar.gz && \
-    cd cvmfs*${cvmfsversion}/ && \
-    mkdir build && \
-    cd build && \
-    cmake .. -DBUILD_SERVER=no -DBUILD_SERVER_DEBUG=no -DDBUILD_SHRINKWRAP=no && \
-    make -j $(nproc) && \
-    make install && \
-    cd ../.. && \
-    rm -r cvmfs*${cvmfsversion}*
-
-    yum remove -y fuse && yum install -y fuse3
+    yum install -y ./ppc64le/cvmfs-2.8.1-1.el7.ppc64le.rpm ./ppc64le/cvmfs-fuse3-2.8.1-1.el7.x86_64.rpm
 else
     yum install -y http://cvmrepo.web.cern.ch/cvmrepo/yum/cvmfs-release-latest.noarch.rpm \
     && yum install -y cvmfs-${cvmfsversion} cvmfs-config-default cvmfs-fuse3 sudo vim openssh-clients
