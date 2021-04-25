72c97f20c0bc688e539c16f86ed311bc083d6c4b
Bob Dröge <b.e.droge@rug.nl>
2021-04-25 13:37:26 +0200

    install cvmfs-config-default on ppc64le

--- containers/install-cvmfs.sh
+++ containers/install-cvmfs.sh
@@ -3,7 +3,9 @@ arch=$(uname -m)
 
 if [ "$arch" = "ppc64le" ]
 then
-    yum install -y /root/rpmbuild/RPMS/ppc64le/cvmfs-${cvmfsversion}-1.el7.ppc64le.rpm ./ppc64le/cvmfs-fuse3-${cvmfsversion}-1.el7.ppc64le.rpm
+    yum install -y /root/rpmbuild/RPMS/ppc64le/cvmfs-${cvmfsversion}-1.el7.ppc64le.rpm \
+                   /root/rpmbuild/RPMS/ppc64le/cvmfs-fuse3-${cvmfsversion}-1.el7.ppc64le.rpm \
+                   http://ecsft.cern.ch/dist/cvmfs/cvmfs-config/cvmfs-config-default-latest.noarch.rpm
 else
     yum install -y http://cvmrepo.web.cern.ch/cvmrepo/yum/cvmfs-release-latest.noarch.rpm \
     && yum install -y cvmfs-${cvmfsversion} cvmfs-config-default cvmfs-fuse3 sudo vim openssh-clients
