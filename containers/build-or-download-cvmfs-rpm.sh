6a3a96793526b2e4d2e77af05f5098be6b3574e7
Bob Dröge <b.e.droge@rug.nl>
2021-04-25 13:41:42 +0200

    use cvmfsversion variable

--- containers/build-or-download-cvmfs-rpm.sh
+++ containers/build-or-download-cvmfs-rpm.sh
@@ -1,3 +1,4 @@
+cvmfsversion=$1
 arch=$(uname -m)
 
 yum install -y wget
@@ -5,9 +6,9 @@ if [ "$arch" = "ppc64le" ]
 then
     yum install -y epel-release
     yum install -y rpm-build checkpolicy cmake fuse-devel fuse3-devel gcc gcc-c++ golang libattr-devel libcap-devel libuuid-devel openssl-devel python2-devel python2-setuptools selinux-policy-devel valgrind-devel hardlink selinux-policy-targeted
-    wget https://ecsft.cern.ch/dist/cvmfs/cvmfs-2.8.1/cvmfs-2.8.1-1.el7.src.rpm && rpmbuild --rebuild cvmfs-2.8.1-1.el7.src.rpm
+    wget https://ecsft.cern.ch/dist/cvmfs/cvmfs-${cvmfsversion}/cvmfs-${cvmfsversion}-1.el7.src.rpm && rpmbuild --rebuild cvmfs-${cvmfsversion}-1.el7.src.rpm
 else
     mkdir -p /root/rpmbuild/RPMS/${arch}
     cd /root/rpmbuild/RPMS/${arch}
-    wget https://ecsft.cern.ch/dist/cvmfs/cvmfs-2.8.1/cvmfs-2.8.1-1.el7.${arch}.rpm
+    wget https://ecsft.cern.ch/dist/cvmfs/cvmfs-${cvmfsversion}/cvmfs-${cvmfsversion}-1.el7.${arch}.rpm
 fi
